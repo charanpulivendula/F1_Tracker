db78af502b3de8ebb09a3cca12d0403720505d59
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-05-17 18:42:20 -0400

    Delete repuli.github.io directory

--- repuli.github.io/src/Components/NoPage.js
+++
@@ -1,9 +0,0 @@
-const NoPage =()=>{
-    return(
-        <div className="place-items-center">
-            <h1 className="text-2xl">404 Page not found</h1>
-        </div>
-    );
-}
-
-export default NoPage;
