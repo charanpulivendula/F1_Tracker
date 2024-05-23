5fc0f291129e6304c3068ad75d9c39072a883bdf
soicroot <charanpulivendula1999@gmail.com>
2024-05-23 15:14:46 -0400

    server changes

--- IU_Racing_Dashboard/src/Components/RacingMap.js
+++ IU_Racing_Dashboard/src/Components/RacingMap.js
@@ -1,56 +1,69 @@
 import React, { useState } from 'react';
-// Import Google Maps API (assuming you have a valid API key)
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
 import racingmap from '../Assets/racing_map.png';
-const libraries = ['places']; // Add places library for better user experience
+import {DarkModeStyles} from '../Static/DarkModeStyles';
+
+const libraries = ['places'];
+
+
 
 const RacingMap = () => {
-  const [mapType, setMapType] = useState('racing'); // Default to racing map
+  const [mapType, setMapType] = useState('google'); // Default to racing map
 
   const handleMapChange = (event) => {
     setMapType(event.target.value);
   };
 
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLEAPIKEY
+  });
+
   const racingMap = (
-    <div className='w-4/5 ml-24'>
-      {/* racing zoom */}
+    <div className=''>
       <img
         alt="Mask group"
         src="https://c.animaapp.com/IFYG8yp8/img/mask-group.png"
+        width={1399}
+        height={1042}
       />
       <div />
-      <img alt="Group" src={racingmap} />
+      <img
+        alt="Group"
+        src={racingmap}
+        width={1399}
+        height={700}
+      />
     </div>
   );
 
   const googleMap = (
-    <LoadScript
-      googleMapsApiKey={process.env.GOOGLEAPIKEY} // Replace with your actual Google Maps API key
-      libraries={libraries}
-    >
-      {({ isLoaded }) => (
-        <div className='w-4/5 ml-16'>
-          {isLoaded ? (
-            <GoogleMap
-              mapContainerStyle={{ width: '100%', height: '400px' }}
-              zoom={15}
-              center={{ lat: 40.8086127, lng: -86.5254012 }} // Adjust center coordinates as needed
-            >
-              <Marker position={{ lat: 40.8086127, lng: -86.5254012 }} /> {/* Add a marker (optional) */}
-            </GoogleMap>
-          ) : (
-            <div>Loading Google Map...</div>
-          )}
-        </div>
+    <div className=''>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{ width: "650px", height: "730px" }}
+          zoom={19}
+          center={{ lat: 39.18990, lng: -86.52281}}
+          options={{ styles: DarkModeStyles }}
+        >
+          <Marker position={{ lat: 39.1898326, lng: -86.5254004 }} />
+        </GoogleMap>
+      ) : (
+        <div>Loading Google Map...</div>
       )}
-    </LoadScript>
+    </div>
   );
 
   const selectedMap = mapType === 'racing' ? racingMap : googleMap;
 
   return (
-    <div>
-      <select style={{color:"#000"}} value={mapType} onChange={handleMapChange}>
+    <div className='flex-col w-4/5 ml-10 h-full'>
+      <select
+        className=''
+        style={{ color: "#000" }}
+        value={mapType}
+        onChange={handleMapChange}
+      >
         <option value="racing">Racing Map</option>
         <option value="google">Google Map</option>
       </select>
